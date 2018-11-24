9abeed8f0d0e2e012029d775154c4979f4e25ed3
Jozue06 <jozue06@gmail.com>
2018-11-23 23:36:21 -0800

    swiftDice

--- 8balls/ViewController.swift
+++ 8balls/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber : Int = 0
+    @IBOutlet weak var balls: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ballsChange()
     }
 
-
+    
+    @IBAction func ballsButton(_ sender: UIButton) {
+        ballsChange()
+    }
+    
+    func ballsChange(){
+        randomBallNumber = Int(arc4random_uniform(4))
+        balls.image = UIImage(named: ballArray[randomBallNumber])
+    }
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        ballsChange()
+    }
 }
 
