c81207d2f9ad892bedfb804601e5149e33fd9721
Jozue06 <jozue06@gmail.com>
2018-11-23 23:16:25 -0800

    Initial Commit

---
+++ 8balls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  8balls
+//
+//  Created by Joshua McClung on 11/23/18.
+//  Copyright © 2018 Joshua McClung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
